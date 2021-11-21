c667b8a48c6e827d86a995e2e308eb019b7bb3ff
Jia <49358172+craftslab@users.noreply.github.com>
2021-11-21 15:57:32 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-repo init -u git@github.com:distbuild/manifest.git -b master -c -m manifest.xml -v --depth=1
+repo init -u git@github.com:distbuild/manifest.git -b main -c -m manifest.xml -v --depth=1
 repo sync
